4292725648a13da2a1a5cc5191615ce78c9bceac
Ignacio <isampedro@itba.edu.ar>
2021-08-08 14:52:42 -0300

    Added time calculation

--- src/main/java/ar/edu/itba/ss/cellindexmethod/App.java
+++ src/main/java/ar/edu/itba/ss/cellindexmethod/App.java
@@ -1,11 +1,20 @@
 package ar.edu.itba.ss.cellindexmethod;
 
-public class App 
+import java.time.Duration;
+import java.time.Instant;
+
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Input input = getInput();        
+        Input input = getInput();
+
+        Instant start = Instant.now();
+        // time passes
+        Instant end = Instant.now();
+        Duration timeElapsed = Duration.between(start, end);
+        System.out.println(timeElapsed.toHours() + ":" + timeElapsed.toMinutes() + ":" + timeElapsed.toSeconds());
     }
     
     private static Input getInput() {
