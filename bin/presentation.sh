b0317ecc34facdd3646bf14a06f999dbdc798d14
Paula Oseroff <oseroffp@gmail.com>
2021-08-13 02:23:58 -0300

    Change presentation.sh

--- bin/presentation.sh
+++ bin/presentation.sh
@@ -15,14 +15,17 @@
 #    tiempo de ejecucion
 #    figura que muestre las posiciones de todas las particulas y que identifique una de ellas y sus vecinos
 #
+echo ""
 
 java -jar cellindexmethod-0.0.1-SNAPSHOT.jar staticFile=input/static.txt dynamicFile=input/dynamic.txt wallPeriod=false randomize=false sameRadius=false 2> output_exec_time.txt 1> /dev/null
 
 # Lista de vecinos con Cell Index Method
+echo -e "\033[0;32mLista de vecinos con Cell Index Method\033[0m"
 cat output_neighbors_cmi.csv
 echo ""
 
 # Tiempos de Ejecucion
+echo -e "\033[0;32mTiempos de Ejecucion\033[0m"
 cat output_exec_time.txt
 echo ""
 
